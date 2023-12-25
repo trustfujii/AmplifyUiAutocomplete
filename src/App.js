c83a946d396f6ec5ee2444704e59a23010ab0eb7
CodeSandbox <support@codesandbox.io>
2023-12-25 05:54:41 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import "./styles.css";
+import React from "react";
+import { useState } from "react";
+import {
+  Autocomplete as AmplifyAutocomplete,
+  HighlightMatch,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 export default function App() {
+  const [value, setValue] = useState();
+  const options = [
+    { id: "apple", label: "apple", description: "appleText" },
+    { id: "banana", label: "banana", description: "bananaText" },
+    { id: "cherry", label: "cherry", description: "cherryText" },
+    { id: "grape", label: "grape", description: "grapeText" },
+    { id: "kiwis", label: "kiwis", description: "kiwisText" },
+    { id: "lemon", label: "lemon", description: "lemonText" },
+    { id: "mango", label: "mango", description: "mangoText" },
+    { id: "orange", label: "orange", description: "orangeText" },
+    { id: "strawberry", label: "strawberry", description: "strawberryText" },
+  ];
+
+  const optionFilter = (option, value) => {
+    // filter options against description
+    return option?.description?.includes(value);
+  };
+
+  const renderOptions = (option, value) => {
+    return (
+      <HighlightMatch style={{ width: "300px" }} query={value}>
+        {option?.description}
+      </HighlightMatch>
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div style={{ width: "300px" }}>Amplify UI Autocomplete</div>
+      <AmplifyAutocomplete
+        value={value ? value.label : ""}
+        width={300}
+        label="Default autocomplete"
+        options={options}
+        optionFilter={optionFilter}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+        onSelect={(option) => {
+          console.log(option);
+          setValue(option);
+        }}
+        onClear={() => {
+          setValue("");
+        }}
+        renderOption={renderOptions}
+      />
     </div>
   );
 }
